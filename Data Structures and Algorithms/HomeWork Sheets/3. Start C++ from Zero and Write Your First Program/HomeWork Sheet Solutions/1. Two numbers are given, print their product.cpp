7b5137cc584c9cbb4c0639cbccab0d58a630f844
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-20 21:09:43 +0530

    Completed

---
+++ Data Structures and Algorithms/HomeWork Sheets/3. Start C++ from Zero and Write Your First Program/HomeWork Sheet Solutions/1. Two numbers are given, print their product.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int a,b;
+    cin>>a>>b;
+    cout<<a*b;
+    return 0;
+}
