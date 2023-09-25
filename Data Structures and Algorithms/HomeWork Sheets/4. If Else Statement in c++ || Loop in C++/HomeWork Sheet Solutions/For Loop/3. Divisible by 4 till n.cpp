f12209fde21aa50bc93c9cc52cf68096287c240d
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-25 11:26:41 +0530

    Completed

---
+++ Data Structures and Algorithms/HomeWork Sheets/4. If Else Statement in c++ || Loop in C++/HomeWork Sheet Solutions/For Loop/3. Divisible by 4 till n.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n;
+    cin>>n;
+
+    for(int i=4;i<=n;i=i+4){
+        cout<<i<<endl;
+    }
+  
+    return 0;
+}
