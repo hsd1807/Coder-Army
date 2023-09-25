1f13f16e45b591dceae2c841ae211e40f12a1add
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-25 11:16:21 +0530

    Completed

---
+++ Data Structures and Algorithms/HomeWork Sheets/4. If Else Statement in c++ || Loop in C++/HomeWork Sheet Solutions/If-Else/3. Month Number.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n;
+    cin>>n;
+
+    if(n==1)
+        cout<<"January";
+    else if(n==2)
+        cout<<"February";
+    else if(n==3)
+        cout<<"March";
+    else if(n==4)
+        cout<<"April";
+    else if(n==5)
+        cout<<"May";
+    else if(n==6)
+        cout<<"June";
+    else if(n==7)
+        cout<<"July";
+    else if(n==8)
+        cout<<"August";
+    else if(n==9)
+        cout<<"September";
+    else if(n==10)
+        cout<<"October";
+    else if(n==11)
+        cout<<"November";
+    else
+        cout<<"December";
+
+    return 0;
+}
