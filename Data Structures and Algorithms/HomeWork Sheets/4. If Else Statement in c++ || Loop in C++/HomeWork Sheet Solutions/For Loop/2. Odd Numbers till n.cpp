b109f5363d47d3b0a708724196b6f311b54c8c6f
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-25 11:24:37 +0530

    Completed

---
+++ Data Structures and Algorithms/HomeWork Sheets/4. If Else Statement in c++ || Loop in C++/HomeWork Sheet Solutions/For Loop/2. Odd Numbers till n.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n;
+    cin>>n;
+
+    for(int i=1;i<=n;i=i+2){
+        cout<<i<<endl;
+    }
+  
+    return 0;
+}
