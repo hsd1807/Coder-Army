d3c2fa668a4b0fb2d60bcbf0bb0133f628c009f3
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-25 11:22:42 +0530

    Completed

---
+++ Data Structures and Algorithms/HomeWork Sheets/4. If Else Statement in c++ || Loop in C++/HomeWork Sheet Solutions/For Loop/1. World Cup 2023.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+
+    for(int i=1;i<=20;i++){
+        cout<<"India will win the World Cup 2023\n";
+    }
+
+    return 0;
+}
