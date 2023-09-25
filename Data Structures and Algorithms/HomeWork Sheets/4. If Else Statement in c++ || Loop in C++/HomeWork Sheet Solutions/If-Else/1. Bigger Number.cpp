a521628c73ce2362e0dc5e4700a3370b37ec9317
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-25 11:07:48 +0530

    Completed

---
+++ Data Structures and Algorithms/HomeWork Sheets/4. If Else Statement in c++ || Loop in C++/HomeWork Sheet Solutions/If-Else/1. Bigger Number.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int a,b;
+    cin>>a>>b;
+    if(a>b)
+        cout<<a;
+    else
+        cout<<b;
+    return 0;
+}
