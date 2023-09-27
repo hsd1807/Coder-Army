d34617ffc68c402876b7d47b9d3dcc5eedd906fe
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-27 11:50:20 +0530

    Completed

---
+++ Data Structures and Algorithms/HomeWork Sheets/6. Solve Any Pattern Problem With Simple Trick Part-1/HomeWork Sheet Solutions/Third Pattern.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int row,col;
+
+    for(row=1;row<=5;row++)
+    {
+        for(col=1;col<=6;col++)
+            cout<<col*col*col<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
