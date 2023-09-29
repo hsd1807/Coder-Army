252f0f99464c616785fd1be4add8183657b8c4d1
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-29 20:46:01 +0530

    Completed

---
+++ Data Structures and Algorithms/HomeWork Sheets/5. For Loop in c++ Advance || Prime Number || Factorial || Fibonacci || Sum of n number/HomeWork Sheet Solutions/1. 280 to 250.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int i;
+
+    for(i=280;i>=250;i=i-1)
+        cout<<i<<endl;
+
+    return 0;
+}
