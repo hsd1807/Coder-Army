c987ca41ee8d1960bbca7c8d2dc5fb40ace30185
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-29 20:50:17 +0530

    Completed

---
+++ Data Structures and Algorithms/HomeWork Sheets/5. For Loop in c++ Advance || Prime Number || Factorial || Fibonacci || Sum of n number/HomeWork Sheet Solutions/3. Z to A.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    for(char name='Z';name>='A';name=name-1)
+        cout<<name<<endl;
+    return 0;
+}
