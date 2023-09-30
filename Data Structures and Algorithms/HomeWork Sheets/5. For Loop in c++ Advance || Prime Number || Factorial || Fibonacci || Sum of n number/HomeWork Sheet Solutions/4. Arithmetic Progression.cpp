2ab10ead81f40fe176291abd69bbfbbcd250cded
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-30 20:27:15 +0530

    Completed

---
+++ Data Structures and Algorithms/HomeWork Sheets/5. For Loop in c++ Advance || Prime Number || Factorial || Fibonacci || Sum of n number/HomeWork Sheet Solutions/4. Arithmetic Progression.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    for(int i=220;i<=730;i+=7)
+        cout<<i<<endl;
+    return 0;
+}
