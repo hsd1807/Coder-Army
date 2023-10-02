18c38056470af3c3a701a1a3c6dc987c45211648
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-10-02 21:15:04 +0530

    Completed

---
+++ Data Structures and Algorithms/HomeWork Sheets/010. Operators in C++ with Implementation/HomeWork Sheet Solutions/3. Age Check.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int age;
+    cin>>age;
+
+    if(age>=13&&age<=19)
+        cout<<"Yes";
+    else
+        cout<<"No";
+
+    return 0;
+}
