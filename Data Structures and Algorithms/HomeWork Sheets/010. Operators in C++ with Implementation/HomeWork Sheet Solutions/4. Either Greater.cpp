946b5af22dea315cb348c5945725bfee70030629
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-10-02 21:08:17 +0530

    Completed

---
+++ Data Structures and Algorithms/HomeWork Sheets/010. Operators in C++ with Implementation/HomeWork Sheet Solutions/4. Either Greater.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    if(a>b || a>c)
+        cout<<"Yes";
+    else
+        cout<<"No";
+  
+    return 0;
+}
