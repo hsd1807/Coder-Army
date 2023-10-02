c8f9bc9be7b4fea8ceee579bbc77dc7c5c58d257
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-10-02 21:12:58 +0530

    Completed

---
+++ Data Structures and Algorithms/HomeWork Sheets/010. Operators in C++ with Implementation/HomeWork Sheet Solutions/2. Even and Positive.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n;
+    cin>>n;
+
+    if(n>0&&n%2==0)
+        cout<<"Yes";
+    else
+        cout<<"No";
+
+    return 0;
+}
