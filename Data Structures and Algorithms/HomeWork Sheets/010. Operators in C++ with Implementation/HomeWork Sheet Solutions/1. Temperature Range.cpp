73aab6f0561ad62733a0aff89b251bbfbc808fe5
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-10-02 21:10:55 +0530

    Completed

---
+++ Data Structures and Algorithms/HomeWork Sheets/010. Operators in C++ with Implementation/HomeWork Sheet Solutions/1. Temperature Range.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int temp;
+    cin>>temp;
+
+    if(temp>70&&temp<90)
+        cout<<"Yes";
+    else
+        cout<<"No";
+
+    return 0;
+}
