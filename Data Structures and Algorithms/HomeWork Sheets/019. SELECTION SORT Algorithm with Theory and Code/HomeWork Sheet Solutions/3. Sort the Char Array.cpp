712efe5225338e843a48a303f61826044324aa9b
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-10-16 20:30:01 +0530

    Completed

---
+++ Data Structures and Algorithms/HomeWork Sheets/019. SELECTION SORT Algorithm with Theory and Code/HomeWork Sheet Solutions/3. Sort the Char Array.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    char arr[100];
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++) cin>>arr[i];
+
+    for(int i=0;i<n-1;i++){
+        int index=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[index])
+                index=j;
+        }
+        swap(arr[i],arr[index]);
+    }
+
+    for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+    return 0;
+}
