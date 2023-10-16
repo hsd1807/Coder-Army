8d21ea488a44d164f611168742de2a1438f6b93c
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-10-16 20:24:56 +0530

    Completed

--- Data Structures and Algorithms/HomeWork Sheets/019. SELECTION SORT Algorithm with Theory and Code/HomeWork Sheet Solutions/1. Decreasing Order.cpp
+++ Data Structures and Algorithms/HomeWork Sheets/019. SELECTION SORT Algorithm with Theory and Code/HomeWork Sheet Solutions/1. Decreasing Order.cpp
@@ -13,7 +13,7 @@ int main()
 
     for(int i=0;i<n-1;i++){
         int index=i;
-        for(int j=i;j<n;j++){
+        for(int j=i+1;j<n;j++){
             if(arr[j]>arr[index])
                 index=j;
         }
