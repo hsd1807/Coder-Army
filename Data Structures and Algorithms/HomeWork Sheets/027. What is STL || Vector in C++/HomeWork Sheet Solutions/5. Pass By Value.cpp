cb7b9e4f95c6f7aa1014f3ad430c3655c409fa32
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-10-24 21:28:01 +0530

    Completed

---
+++ Data Structures and Algorithms/HomeWork Sheets/027. What is STL || Vector in C++/HomeWork Sheet Solutions/5. Pass By Value.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fun(vector<int> v)
+{
+    v.push_back(13);
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    vector<int> vect;
+    vect.push_back(7);
+    fun(vect);
+    for(int i: vect)
+        cout<<i;
+    return 0;
+}
