ebd2bbc4c13ea303a9e31445f09e2f84c23f43ea
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-12-08 15:56:26 +0530

    Completed

---
+++ Data Structures and Algorithms/HomeWork Sheets/050. Dynamic Memory Allocation of 2D and 3D Arrays in C++/HomeWork Sheet Solutions/2. Deallocate 3D Array.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int L,B,H;
+    cin>>L>>B>>H;
+    /* Create 3D Array */
+    int ***ptr=new int **[B];
+    for(int i=0;i<L;i++){
+        ptr[i]=new int *[B];
+        for(int j=0;j<B;j++){
+            ptr[i][j]=new int [H];
+        }
+    }
+    /* Deallocate 3D Array */
+    for(int i=0;i<L;i++){
+        for(int j=0; j<B;j++){
+            delete [] ptr[i][j];
+        }
+        delete [] ptr[i];
+    }
+    delete ptr;
+    return 0;
+}
