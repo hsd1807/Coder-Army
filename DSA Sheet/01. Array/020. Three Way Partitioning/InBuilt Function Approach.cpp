3175509811f508126ba114ad12288818b7a7ce23
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-02-07 12:42:35 +0530

    Completed

---
+++ DSA Sheet/01. Array/020. Three Way Partitioning/InBuilt Function Approach.cpp
@@ -0,0 +1,8 @@
+class Solution{   
+public:
+    //Function to partition the array around the range such that array is divided into three parts.
+    void threeWayPartition(vector<int>& array,int a, int b)
+    {
+        sort(array.begin(), array.end());
+    }
+};
